7f520d953ac6bf82cc2fe0160244c8f218241e75
Assadi <assadi.halifa@gmail.com>
2021-09-23 12:31:04 +0200

    update card

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,5 @@
 const deleteBtn = document.querySelectorAll(".deleteModalBtn");
-const localHost = location.protocol + "//" + location.host;
+//const localHost = location.protocol + "//" + location.host;
 
 //Recupere le lien de l'element à supprimer de l'attribut href parent et l'inserer dans le l'attribut href du boutton du modal
 const deletRedirect = (e) => {
